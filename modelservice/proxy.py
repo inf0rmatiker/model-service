97fc3646c9d29238dd2a69f4c9061896e6cbc3dc
Caleb Carlson <ccarlson355@gmail.com>
2022-04-03 15:47:00 -0600

    Set up project skeleton

--- modelservice/proxy.py
+++ modelservice/proxy.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 # Main entrypoint
 def run(master_hostname="localhost", master_port=50051, flask_port=5000):
     print("Running flask server...")
@@ -9,11 +10,13 @@ def run(master_hostname="localhost", master_port=50051, flask_port=5000):
     app.config["MASTER_PORT"] = master_port
     app.run(host="0.0.0.0", port=flask_port)  # Entrypoint
 
+
 @app.route("/model", methods=["GET"])
 def get_model():
     return "At some point, your model will be returned from this endpoint"
 
-@app.route("/submit-job", methods=["POST"])
+
+@app.route("/model", methods=["POST"])
 def submit_job():
     request_data: str = request.json
     print(f"request_data: {request_data}")
