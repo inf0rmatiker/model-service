421d41548ee494a00731a960ed8850c06ddf3d3c
NFabrizio <nicholas.fabrizio@gmail.com>
2022-04-10 14:34:58 -0600

    feature/worker-registration Adds initial updates for master register method

--- modelservice/master.py
+++ modelservice/master.py
@@ -8,14 +8,44 @@ from modelservice.modelservice_pb2 import WorkerRegistrationRequest, WorkerRegis
     GetModelRequest, GetModelResponse, BuildModelsResponse
 
 
+class WorkerMetadata:
+
+    def __init__(self, hostname, port, shard):
+        self.hostname = hostname
+        self.port = port
+
+    def __repr__(self):
+        return f"WorkerMetadata: hostname={self.hostname}, port={self.port}"
+
+
 # Master Service
 class Master(modelservice_pb2_grpc.MasterServicer):
 
-    def __init__(self):
+    def __init__(self, hostname: str, port: int):
         super(Master, self).__init__()
+        self.hostname = hostname
+        self.port = port
+
+        # Data structures
+        self.tracked_workers = {}       # Mapping of { hostname -> WorkerMetadata }
+        self.gis_join_locations = {}    # Mapping of { gis_join -> WorkerMetadata }
+
+    def is_worker_registered(self, hostname):
+        return hostname in self.tracked_workers
 
     def RegisterWorker(self, request: WorkerRegistrationRequest, context):
         info(f"Received request to register worker: hostname={request.hostname}, port={request.port}")
+
+        # Create a WorkerMetadata object for tracking
+        worker: WorkerMetadata = WorkerMetadata(request.hostname, request.port)
+        # info(f"Successfully added Worker: {worker}, responsible for {len(shard.gis_join_metadata)} GISJOINs")
+        # Add worker metadata to tracked_workers map for easy reference later
+        self.tracked_workers[request.hostname] = worker
+
+        # Loop through list of GIS joins and store their ID and metadata about worker that is hosting them
+        for local_gis_join in request.local_gis_joins:
+            self.gis_join_locations[local_gis_join.gis_join] = worker
+
         return WorkerRegistrationResponse(success=True)
 
     def DeregisterWorker(self, request, context):
