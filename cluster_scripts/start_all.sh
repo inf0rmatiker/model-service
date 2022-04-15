af3f09af8c988ab5eb59862efd0bc43c7a7f027c
Caleb Carlson <ccarlson355@gmail.com>
2022-04-15 15:43:23 -0600

    Fix start_all.sh

--- cluster_scripts/start_all.sh
+++ cluster_scripts/start_all.sh
@@ -6,17 +6,20 @@ function print_usage {
   echo -e "\tPlease make sure you have a 'workers' file, with the hostnames of each worker separated by newlines"
 }
 
-CURR_DIR=$(pwd)
-VALIDATION_SERVICE_DIR="$HOME/model-service"
+[[ ! -f "./env.sh" ]] && echo -e "No env.sh script found! Please create one using env.sh.example" && exit 1
+source ./env.sh
 
-cd "$VALIDATION_SERVICE_DIR" || exit 1
+CURR_DIR=$(pwd)
+MODEL_SERVICE_DIR="$HOME/model-service"
+
+cd "$MODEL_SERVICE_DIR" || exit 1
 echo -e "Starting Master..."
-nohup "./run_master.sh" 50051 > "master_$(hostname).log" & disown
+nohup ./run_master.sh "$MASTER_PORT" > "master_$(hostname).log" & disown
 sleep 5
 echo -e "Starting Proxy..."
-nohup "./run_proxy.sh" "$(hostname):50051" 5000 > "flask_server_$(hostname).log" & disown
+nohup ./run_proxy.sh "$(hostname):$MASTER_PORT" "$PROXY_PORT" > "proxy_$(hostname).log" & disown
 sleep 1
 
 cd "$CURR_DIR" || exit 1
 echo -e "Starting Workers..."
-./start_workers.sh "$(hostname):50051" "$WORKERS_FILE"
+./start_workers.sh "$(hostname):$MASTER_PORT" "$WORKERS_FILE"
