04695b3ffd293f6c2072179cd424a2023caf6c68
Caleb Carlson <ccarlson355@gmail.com>
2022-04-14 20:07:52 -0600

    Actually scp files

--- distribute_data.sh
+++ distribute_data.sh
@@ -17,6 +17,7 @@ fi
 
 TEMP_LOC="/tmp/model_service"
 [[ ! -d $TEMP_LOC ]] && mkdir $TEMP_LOC && tar -xvf "$ORIGINAL_TAR_FILE" -C "$TEMP_LOC"
+chmod 777 $TEMP_LOC $TEMP_LOC/*.csv
 
 readarray -t WORKERS < ./workers
 NUM_WORKERS=${#WORKERS[@]}
@@ -24,7 +25,7 @@ NUM_FILES=$(ls $TEMP_LOC/*.csv | wc -l)
 FILES_PER_WORKER=$((NUM_FILES / NUM_WORKERS))
 while read -r WORKER; do
   BATCH=$(ls $TEMP_LOC/*.csv | head -n $FILES_PER_WORKER)
-  ssh -n "$WORKER" "mkdir /tmp/model_service"
+  ssh -n "$WORKER" "mkdir /tmp/model_service && chmod 777 /tmp/model_service"
   for CSV_FILE in ${BATCH[@]}; do
     echo -e "Sending $CSV_FILE to $WORKER..."
     scp "$CSV_FILE" "$WORKER:/tmp/model_service/"
