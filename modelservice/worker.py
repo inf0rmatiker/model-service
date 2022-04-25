420185cf0176b5e1ebcc73e577c1be9b81b4d58e
rushabh10shah <42091092+rushabh10shah@users.noreply.github.com>
2022-04-24 19:12:39 -0600

    Update worker.py

--- modelservice/worker.py
+++ modelservice/worker.py
@@ -10,7 +10,7 @@ from logging import info, error
 from modelservice.modelservice_pb2 import BuildModelsRequest, BuildModelsResponse, GetModelRequest, GetModelResponse, \
     GisJoinMetadata, WorkerRegistrationResponse, WorkerRegistrationRequest, WorkerBuildModelsResponse
 
-
+shared_executor = get_reusable_executor(max_workers=10, timeout=10)
 class Worker(modelservice_pb2_grpc.WorkerServicer):
 
     def __init__(self, master_hostname: str, master_port: int, hostname: str, port: int, data_dir: str):
@@ -72,6 +72,19 @@ class Worker(modelservice_pb2_grpc.WorkerServicer):
 
     def BuildModels(self, request: BuildModelsRequest, context) -> WorkerBuildModelsResponse:
         info(f"Received request to build models: {request}")
+        
+        # Save model
+        if not self.save_model(request):
+            return WorkerBuildModelsResponse(ok=False, hostname=self.hostname)
+			
+		tf_validator: TensorflowValidator = TensorflowValidator(request, shared_executor, self.local_gis_joins)
+		metrics = tf_validator.validate_gis_joins()
+
+        else:
+            return WorkerBuildModelsResponse(ok=False, hostname=self.hostname, error_msg="Building models currently unimplemented")
+
+        # Create and return response from aggregated metrics
+        
         return BuildModelsResponse(
             id=request.id,
             hostname=self.hostname,
@@ -79,6 +92,38 @@ class Worker(modelservice_pb2_grpc.WorkerServicer):
             error_occurred=True,
             error_msg="Building models currently unimplemented"
         )
+    
+    def save_model(self, request: BuildModelsRequest) -> bool:
+        ok = True
+
+        # Make the directory
+        model_dir = f"{self.saved_models_path}/{request.id}"
+        os.mkdir(model_dir)
+        info(f"Created directory {model_dir}")
+
+        file_extension = "pkl"  # Default for Scikit-Learn pickle type
+
+        # Validate the file type with the framework
+        
+        # Saved Tensorflow models have to be either SavedModel or HDF5 format:
+        # https://www.tensorflow.org/tutorials/keras/save_and_load#save_the_entire_model
+        if request.model_file.type == ModelFileType.TENSORFLOW_SAVED_MODEL_ZIP:
+            zip_file = zipfile.ZipFile(io.BytesIO(request.model_file.data))
+            zip_file.extractall(model_dir)
+            return ok
+        elif request.model_file.type == ModelFileType.TENSORFLOW_HDF5:
+            file_extension = "h5"
+        else:
+            return not ok
+
+
+        # Save the model with appropriate extension
+        model_file_path = f"{model_dir}/{request.id}.{file_extension}"
+        with open(model_file_path, "wb") as binary_file:
+            binary_file.write(request.model_file.data)
+
+        info(f"Finished saving model to {model_file_path}")
+        return ok
 
     def GetModel(self, request: GetModelRequest, context) -> GetModelResponse:
         info(f"Received request to retrieve model(s)")
